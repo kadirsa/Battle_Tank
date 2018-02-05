75467900f5e1196d3f31eb23acb8c1fb48f3877b
Kadir <Kadirmuti@hotmail.com>
2018-02-05 15:49:51 +0300

    Yeni map oluşturuldu

---
+++ BattleTankWar/Source/BattleTankWar/BattleTankWarGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTankWar.h"
+#include "BattleTankWarGameModeBase.h"
+
+
+
+
---
+++ BattleTankWar/Source/BattleTankWar/BattleTankWarGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankWarGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKWAR_API ABattleTankWarGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
